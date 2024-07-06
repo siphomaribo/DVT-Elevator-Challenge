84278e140674f42364b53751ceda78c0d1a8a71b
Sipho Maribo <mpho@mcmaribo.co.za>
2024-07-06 14:20:20 +0200

    Add depency injection to the the start up/program class

--- DVT.Elevator.Challenge/Program.cs
+++ DVT.Elevator.Challenge/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DVT.Elevator.Challenge.Domain.Abstraction.Interface;
+using DVT.Elevator.Challenge.Domain.Service;
+using Microsoft.Extensions.DependencyInjection;
+using DVT.Elevator.Challenge.Domain.Model;
+
+var serviceProvider = new ServiceCollection()
+                .AddSingleton<IElevatorService>(sp => new ElevatorService(10, new List<ElevatorModel>
+                {
+                new ElevatorModel() { Id = 1, CurrentFloor = 1, Direction = ElevatorDirectionEnum.Idle, Capacity = 10, Type = ElevatorTypeEnum.Passenger },
+                new ElevatorModel() { Id = 2, CurrentFloor = 5, Direction = ElevatorDirectionEnum.Up, Capacity = 8, Type = ElevatorTypeEnum.Passenger },
+                new ElevatorModel() { Id = 3, CurrentFloor = 10, Direction = ElevatorDirectionEnum.Down, Capacity = 12, Type = ElevatorTypeEnum.HighSpeed },
+                new ElevatorModel() { Id = 4, CurrentFloor = 15, Direction = ElevatorDirectionEnum.Down, Capacity = 15, Type = ElevatorTypeEnum.Freight }
+                }))
+                .BuildServiceProvider();
+
+var elevatorService = serviceProvider.GetService<IElevatorService>();
+
 Console.WriteLine("Hello, World!");
